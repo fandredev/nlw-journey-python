4dabeb9904c02965c5168ca6b2e5019eb9e3fc88
felipe <profissionalf.andre@gmail.com>
2024-07-09 16:37:15 -0300

    feat: add LinkCreator and TripConfirm

---
+++ src/controllers/link_creator.py
@@ -0,0 +1,35 @@
+from src.models.repositories.links_repository import LinksRepository
+
+from typing import Dict
+from http import HTTPStatus
+
+from uuid import uuid4
+
+
+class LinkCreator:
+    def __init__(self, link_repository: LinksRepository) -> None:
+        self.__link_repository = link_repository
+
+    def create(self, body, trip_id: str) -> Dict:
+        try:
+            link_id = str(uuid4())
+            link_infos = {
+                "id": link_id,
+                "link": body["link"],
+                "title": body["title"],
+                "trip_id": trip_id,
+            }
+
+            self.__link_repository.registry_link(link_infos)
+
+            return {
+                "body": {
+                    "link_id": link_id,
+                },
+                "status_code": HTTPStatus.CREATED,
+            }
+        except Exception as exception:
+            return {
+                "body": {"message": str(exception), "error": HTTPStatus.BAD_REQUEST},
+                "status_code": HTTPStatus.BAD_REQUEST,
+            }
---
+++ src/controllers/trip_confirm.py
@@ -0,0 +1,22 @@
+from typing import Dict
+from src.models.repositories.trips_repository import TripsRepository
+from http import HTTPStatus
+
+
+class TripConfirm:
+    def __init__(self, trips_repository: TripsRepository) -> None:
+        self.__trips_repository = trips_repository
+
+    def confirm(self, trip_id: str) -> Dict:
+        try:
+            self.__trips_repository.update_trip_status(trip_id)
+
+            return {
+                "body": {"message": "Trip confirmed successfully", "trip_id": trip_id},
+                "status_code": HTTPStatus.NO_CONTENT,
+            }
+        except Exception as exception:
+            return {
+                "body": {"message": str(exception), "error": HTTPStatus.BAD_REQUEST},
+                "status_code": HTTPStatus.BAD_REQUEST,
+            }
